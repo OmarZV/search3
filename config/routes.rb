cd4803e2d4d3f8e94726ec2d192192229c7491bc
OmarZV <oekhedr@yahoo.com>
2017-06-14 05:06:34 -0400

    search2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :movies
+  resources :directors
+
+  get :search, controller: :main
+  root to: "main#index"
 end
